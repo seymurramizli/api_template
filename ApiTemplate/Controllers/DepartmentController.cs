d31c4e80b013b07084029def3088ffa664649425
Ruslan Babali <RBabali@pasha-insurance.az>
2022-04-26 11:52:02 +0400

    ValidatorActionFilter added

--- ApiTemplate/Controllers/DepartmentController.cs
+++ ApiTemplate/Controllers/DepartmentController.cs
@@ -6,19 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiTemplate.Controllers
 {
-    public class DepartmentController : BaseController<DepartmentController>
+    public class DepartmentController : ControllerBase
     {
 
         private readonly IDepartmentService _departmentService;
 
-        public DepartmentController(IDepartmentService departmentService,
-            Common.Logging.IAppLogger<DepartmentController> logger) : base(logger)
+        public DepartmentController(IDepartmentService departmentService)
         {
             _departmentService = departmentService;
         }
 
         [HttpPost("department")]
-        public async Task<ApiResponse> Save(DepartmentDto request)
+        public async Task<ApiResponse> Save([FromBody] DepartmentDto request)
         {
             return await _departmentService.saveDepartment(request);
         }
---
+++ ApiTemplate/DTOs/Validator/DepartmentDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ApiTemplate.DTOs.Validator
+{
+    public class DepartmentDtoValidator : AbstractValidator<DepartmentDto>
+    {
+        public DepartmentDtoValidator()
+        {
+            RuleFor(m => m.Name)
+                .NotEmpty()
+                .WithMessage("Adı daxil edin");
+        }
+    }
+}
